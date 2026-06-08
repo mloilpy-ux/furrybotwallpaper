d42fdacc59659dff96fbb88b8f8ad84f69b9d030
mloilpy-ux <mloilpy@gmail.com>
2026-06-08 22:06:16 +0300

    Update twitter.py

--- bot/services/twitter.py
+++ bot/services/twitter.py
@@ -1,13 +1,14 @@
-import aiohttp, re
-async def parse_twitter(url: str, limit: int = 20):
-    user = url.split("/")[-1].split("?")[0]
-    for host in ["nitter.net","nitter.privacydev.net","nitter.it"]:
-        try:
-            api = f"https://{host}/{user}/rss"
-            async with aiohttp.ClientSession() as s:
-                async with s.get(api, timeout=10) as r:
-                    txt = await r.text()
-                    imgs = re.findall(r'<img src="(https://pbs.twimg.com/media/[^"]+)"', txt)
-                    return [{"title":"","media_url":i+":large","post_url":url,"is_nsfw":False,"is_gif":False} for i in imgs[:limit]]
-        except: continue
-    return []
+import aiohttp
+import re
+from typing import List, Dict
+
+async def parse_twitter(url: str, limit: int = 20) -> List[Dict]:
+    try:
+        user = url.split("/")[-1].split("?")[0]
+        async with aiohttp.ClientSession() as s:
+            async with s.get(f"https://nitter.net/{user}/rss", timeout=10) as r:
+                txt = await r.text()
+        imgs = re.findall(r'https://pbs\.twimg\.com/media/[^"<\s]+', txt)
+        return [{"title": "", "media_url": i, "post_url": url, "is_nsfw": False, "is_gif": False} for i in imgs[:limit]]
+    except:
+        return []
